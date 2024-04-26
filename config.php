135b9d20109cdf6b14d88a729347fcb4111dc67c
Wangu-Phillip <115086151+Wangu-Phillip@users.noreply.github.com>
2024-04-26 11:19:26 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "bwcyber");
+$conn = mysqli_connect("us-cluster-east-01.k8s.cleardb.net", "b101cbed28bf40", "cae46924", "heroku_043e4e0afcd05b6");
 
 // Check if the connection was successful
 if ($conn->connect_error) {
